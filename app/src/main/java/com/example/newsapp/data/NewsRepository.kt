5fcc60c1262a3b41a0a015a6fb98c7d935886d21
devs.agilsantosa <devs.agilsantosa@gmail.com>
2023-02-27 05:35:03 +0700

    Using coroutine

--- app/src/main/java/com/example/newsapp/data/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/data/NewsRepository.kt
@@ -1,71 +1,53 @@
 package com.example.newsapp.data
 
+import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
 import com.example.newsapp.BuildConfig
 import com.example.newsapp.data.local.entity.NewsEntity
 import com.example.newsapp.data.local.room.NewsDao
-import com.example.newsapp.data.remote.response.NewsResponse
 import com.example.newsapp.data.remote.retrofit.ApiService
 import com.example.newsapp.utils.AppExecutors
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class NewsRepository private constructor(
     private val apiService: ApiService,
     private val newsDao: NewsDao,
     private val appExecutors: AppExecutors
 ) {
-
-    private val result = MediatorLiveData<Result<List<NewsEntity>>>()
-
-    fun getHeadLineNews(): LiveData<Result<List<NewsEntity>>> {
-        result.value = Result.Loading
-        val client = apiService.getNews(BuildConfig.API_KEY)
-        client.enqueue(object : Callback<NewsResponse> {
-            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
-                if (response.isSuccessful) {
-                    val articles = response.body()?.articles
-                    val newsList = ArrayList<NewsEntity>()
-                    appExecutors.diskIO.execute {
-                        articles?.forEach { article ->
-                            val isBookmarked = newsDao.isNewsBookmarked(article.title)
-                            val news = NewsEntity(
-                                article.title,
-                                article.publishedAt,
-                                article.urlToImage,
-                                article.url,
-                                isBookmarked
-                            )
-                            newsList.add(news)
-                        }
-                        newsDao.deleteAll()
-                        newsDao.insertNews(newsList)
-                    }
-                }
+    fun getHeadLineNews(): LiveData<Result<List<NewsEntity>>> = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.getNews(BuildConfig.API_KEY)
+            val articles = response.articles
+            val newList = articles.map { article ->
+                val isBookmarked = newsDao.isNewsBookmarked(article.title)
+                NewsEntity(
+                    article.title,
+                    article.publishedAt,
+                    article.urlToImage,
+                    article.url,
+                    isBookmarked
+                )
             }
-
-            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                result.value = Result.Error(t.message.toString())
-            }
-        })
-        val localData = newsDao.getNews()
-        result.addSource(localData) { newData: List<NewsEntity> ->
-            result.value = Result.Success(newData)
+            newsDao.deleteAll()
+            newsDao.insertNews(newList)
+        } catch (e: Exception) {
+            Log.d("News Repositoty", "getHeadlineNews: ${e.message.toString()}")
+            e.printStackTrace()
         }
-        return result
+        val localData: LiveData<Result<List<NewsEntity>>> =
+            newsDao.getNews().map { Result.Success(it) }
+        emitSource(localData)
     }
 
     fun getBookmarkedNews(): LiveData<List<NewsEntity>> {
         return newsDao.getBookmarkedNews()
     }
 
-    fun setBookmarkedNews(news: NewsEntity, bookmarkedState: Boolean) {
-        appExecutors.diskIO.execute {
-            news.isBookmarked = bookmarkedState
-            newsDao.updateNews(news)
-        }
+    suspend fun setBookmarkedNews(news: NewsEntity, bookmarkedState: Boolean) {
+        news.isBookmarked = bookmarkedState
+        newsDao.updateNews(news)
     }
 
     companion object {
