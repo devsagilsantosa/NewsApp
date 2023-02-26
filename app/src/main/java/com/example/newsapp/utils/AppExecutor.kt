ece964923e8e6e7721ed504bad86d65a209f55ee
devs.agilsantosa <devs.agilsantosa@gmail.com>
2023-02-26 08:20:12 +0700

    starter for DI

---
+++ app/src/main/java/com/example/newsapp/utils/AppExecutor.kt
@@ -0,0 +1,19 @@
+package com.example.newsapp.utils
+
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class AppExecutor {
+    val diskIO: Executor = Executors.newSingleThreadExecutor()
+    val networkIO: Executor = Executors.newFixedThreadPool(3)
+    val mainThread: Executor = MainThreadExecutor()
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = android.os.Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+}
